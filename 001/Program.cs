876752d3df8658b7b385391d2b72275c4ac136c4
Anna <annechkap@list.ru>
2022-11-15 01:22:35 +0300

    Добавлена задача 001

---
+++ 001/Program.cs
@@ -0,0 +1,51 @@
+﻿// 1. Задайте двумерный массив. Напишите программу, которая упорядочивает по убыванию элементы каждой строки двумерного массива.
+
+int[,] CreateArray(int lenRows, int lenColumns)
+{
+    int[,] array = new int[lenRows, lenColumns];
+    Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(1, 50);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array = CreateArray(3, 4);
+PrintArray(array);
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1) - 1; j++)
+    {
+        for (int z = 0; z < array.GetLength(1) - 1; z++)
+        {
+            if (array[i, z] < array[i, z + 1]) //для изменения сортировки поменять знак < на противоположный
+            {
+                int temp = 0;
+                temp = array[i, z];
+                array[i, z] = array[i, z + 1];
+                array[i, z + 1] = temp;
+            }
+        }
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine("Массив с упорядоченными значениями");
+PrintArray(array);
